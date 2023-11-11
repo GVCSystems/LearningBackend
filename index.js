a4c63add967945a69565e4b2fb16bced2c0f1156
GVC Systems <documents@gvc.in>
2023-11-11 17:55:25 +0530

    adding users.js and trying sequalize

--- index.js
+++ index.js
@@ -2,4 +2,12 @@ const express=require('express');
 const app=express();
 require ('./database.js');
 console.log('app started on port 3400');
+
+// try and see if this works
+const users = await User.findAndCountAll({
+    order: [['createdAt', 'DESC'], ['firstName', 'ASC']],
+    offset: (page - 1) * limit,
+    limit,
+  });
+
 app.listen(3400);
